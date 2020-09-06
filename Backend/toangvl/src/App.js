8cfef495d05e34d970fed3d21ce90bc8450781b2
Duy <duynguyenngoc@hotmail.com>
2020-09-06 12:56:45 +0700

    initial

--- Backend/toangvl/src/App.js
+++ Backend/toangvl/src/App.js
@@ -2,30 +2,30 @@
 import express from 'express'
 
 // App Imports
-import database from './setup/server/database'
-import middlewares from './setup/server/middlewares'
-import upload from './setup/server/upload'
-import endpoint from './setup/server/endpoint'
+// import database from './setup/server/database'
+// import middlewares from './setup/server/middlewares'
+// import upload from './setup/server/upload'
+// import endpoint from './setup/server/endpoint'
 import start from './setup/server/start'
-import socket from './setup/server/socket'
+// import socket from './setup/server/socket'
 
 // Create express server
 const server = express()
 
 // Connect database
-database()
+// database()
 
 // Setup middlewares
-middlewares(server)
+// middlewares(server)
 
 // Setup uploads
-upload(server)
+// upload(server)
 
 // Setup endpoint
-endpoint(server)
+// endpoint(server)
 
 // Setup Socket Io
-socket(server)
+// socket(server)
 
 // Start server
 start(server)
