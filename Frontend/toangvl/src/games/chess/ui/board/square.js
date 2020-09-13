08065cb55926271537751224e426db9e4748f85a
Duy <duynguyenngoc@hotmail.com>
2020-09-13 13:07:32 +0700

    [FE] move quan co nhung chua set turn

--- Frontend/toangvl/src/games/chess/ui/board/square.js
+++ Frontend/toangvl/src/games/chess/ui/board/square.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import '../index.css';
 
+
 export default function Square(props) {
     return (
-      <button className={"square " + props.shade}
-        onMouseEnter={props.onMouseEnter}
-        onMouseLeave={props.onMouseLeave}
-        style={props.style}>
+      <button onClick={props.onClick} className={"square " + props.shade}
+        // onMouseEnter={props.onMouseEnter}
+        // onMouseLeave={props.onMouseLeave}
+        style={props.style} 
+        >
       </button>
+      
     );
   
 }
--- Frontend/toangvl/src/games/chess/ui/chess.js
+++ Frontend/toangvl/src/games/chess/ui/chess.js
@@ -3,38 +3,82 @@ import React from 'react';
 
 // Imports App
 import Board from './board/board.js';
-import './index.css'
-import InitialBoard  from '../helper/initialBoard.js'
+import './index.css';
+import InitialBoard  from '../helper/initialBoard.js';
+import Empty from '../chessman/empty.js';
 
 export default class Chess extends React.Component {
 
   constructor(){
+   
     super();
     this.state = {
       squares: InitialBoard(),
       player: 1,
-      suggest: {status: 0, dict: []}
+      suggest: {status: 0, dict: []},
+      click: 0,
+      location: -1,
     }
   }
 
-  // neu squares == null co nghia la con tro chuot khong nam tren vi tri co quan co
-  onMouseEnter(i){
+  // draw Suggest
+  drawSuggest(i, squares, suggest){
+    console.log("[Sugesst] Cac vi tri co the click: ", suggest)
+    this.setState({squares: squares, location: i, suggest: {status: 1, dict: suggest}});
+  
+  }
+
+  // removeSugest
+  removeSuggest(i, squares, suggest) {
+    console.log("[Remove Sugesst] Cac vi tri co the remove: ", suggest)
+    this.setState({squares: squares, location: -1, suggest: {status: -1, dict: suggest}});
+  }
+
+  // handleMoveChessMan
+  moveCheckMan(start, end){
+    console.log("[Move Chessman] move from %s to %s starting....", start, end);
     const squares = this.state.squares.slice();
-    if (!this.state.squares[i].empty) {
-      const suggest = squares[i].isMovePossible(i);
-      this.setState({squares: squares, location: i, suggest: {status: 1, dict: suggest}});
-    }
+    squares[end] = squares[start];
+    squares[start] = new Empty();
+    this.setState({squares: squares});
+    console.log("[Move Chessman] ....stop");
   }
-  onMouseLeave(i) {
+
+  // handleClick
+  onClickHandle(i){
     const squares = this.state.squares.slice();
-    if (!this.state.squares[i].empty) {
-      const suggest = squares[i].isMovePossible(i);
-      this.setState({squares: squares, location: i, suggest: {status: -1, dict: suggest}});
+    if(this.state.click === 0){
+      if (!this.state.squares[i].empty){
+        console.log("[Click: 1] Trong vung cho phep")
+        const suggest = squares[i].isMovePossible(i);
+        this.drawSuggest(i,squares, suggest);
+        this.setState(this.setState({click: 1}));
+      }else{
+        console.log("[Click: 1] Ngoai vung cho phep")
+      }
     }
+    else{
+      if (this.state.suggest.dict.indexOf(i) >= 0){
+        console.log("[Click: 2] Trong vung cho phep")
+        if (!this.state.squares[i].empty){
+          console.log("[Click: 2] Click vao chinh no -> remove goi y")
+          const suggest = squares[i].isMovePossible(i);
+          this.removeSuggest(i,squares, suggest);
+          this.setState(this.setState({click: 0, suggest: {status: 0, dict: []}}));
+        }else{
+          console.log("[Click: 2] Move quan co")
+          this.moveCheckMan(this.state.location, i);
+          this.setState(this.setState({click: 0, suggest: {status: 0, dict: []}, location: -1}));
+        }
+      }
+      else{
+        console.log("[Click: 2] Ngoai vung cho phep")
+      }
+    }
+
+    
   }
-
-
-
+    
   render() {
         return (
           <div>
@@ -46,13 +90,11 @@ export default class Chess extends React.Component {
                 <Board
                   squares = {this.state.squares}
                   suggest = {this.state.suggest}
-                  onMouseEnter={(i) => this.onMouseEnter(i)}
-                  onMouseLeave={(i) => this.onMouseLeave(i)}
+                  // onMouseEnter={(i) =>  this.onMouseEnter(i)}
+                  // onMouseLeave={(i) =>  this.onMouseLeave(i)}
+                  onClick= {(i) =>  this.onClickHandle(i)}
                 />
               </div>
-              {/* <div className="rank-info">
-                Here is a rank
-              </div> */}
             </div>
           </div>
          
